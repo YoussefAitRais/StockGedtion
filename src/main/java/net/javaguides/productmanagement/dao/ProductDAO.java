6d73b29e64aae04a43d2db10b17f5bed4dcc3b22
Youssef Ait Rais <copybuisness007@gmail.com>
2025-02-14 16:29:18 +0200

    packagejava

--- src/main/java/net/javaguides/productmanagement/dao/ProductDAO.java
+++ src/main/java/net/javaguides/productmanagement/dao/ProductDAO.java
@@ -3,8 +3,13 @@ package net.javaguides.productmanagement.dao;
 
 import java.sql.Connection;
 import java.sql.Statement;
-import java.sql.DriverAction;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductDAO  {
     private static final String URL = "jdbc:mysql://localhost:8083/produit";
     private static final String USER = "root";
