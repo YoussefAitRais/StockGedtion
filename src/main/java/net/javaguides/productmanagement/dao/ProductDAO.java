bba1c20c69b179fa64f3c186b3ace938bdcf76c4
Youssef Ait Rais <copybuisness007@gmail.com>
2025-02-14 15:14:34 +0200

    updatesdao

--- src/main/java/net/javaguides/productmanagement/dao/ProductDAO.java
+++ src/main/java/net/javaguides/productmanagement/dao/ProductDAO.java
@@ -1,4 +1,15 @@
 package net.javaguides.productmanagement.dao;
 
+
+import java.sql.Connection;
+import java.sql.DriverAction;
+import java.sql.DriverManager;
+
 public class ProductDAO  {
+    public static void main(String[] args) {
+
+        //Connection connection = DriverManager.getConnection();
+    }
+
+
 }
