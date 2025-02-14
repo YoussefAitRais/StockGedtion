8561869188e3c3295d172c9e6fce68667cfba266
Youssef Ait Rais <copybuisness007@gmail.com>
2025-02-14 16:04:29 +0200

    DAOupdates

--- src/main/java/net/javaguides/productmanagement/dao/ProductDAO.java
+++ src/main/java/net/javaguides/productmanagement/dao/ProductDAO.java
@@ -2,14 +2,30 @@ package net.javaguides.productmanagement.dao;
 
 
 import java.sql.Connection;
+import java.sql.Statement;
 import java.sql.DriverAction;
 import java.sql.DriverManager;
-
 public class ProductDAO  {
+    private static final String URL = "jdbc:mysql://localhost:8083/produit";
+    private static final String USER = "root";
+    private static final String PASSWORD = "admin";
+
     public static void main(String[] args) {
 
-        //Connection connection = DriverManager.getConnection();
-    }
+        ProductDAO dao = new ProductDAO();
+        Connection conn = null;
+        Statement stmt = null;
+
+        try {
+
+        }
+
+
+
+
+
+
+}
 
 
 }
--- src/main/java/net/javaguides/productmanagement/model/Product.java
+++ src/main/java/net/javaguides/productmanagement/model/Product.java
@@ -1,4 +1,23 @@
 package net.javaguides.productmanagement.model;
 
+
+
 public class Product {
-}
+    protected int id;
+    protected String productNAme;
+    protected String description;
+    protected String quantity;
+    protected String prix_unitaire;
+    protected String category;
+
+    public Product() {}
+
+    public Product(String productNAme, String description, String quantity, String prix_unitaire, String category ) {
+        super();
+        this.productNAme  = productNAme;
+        this.description = description;
+        this.quantity = quantity;
+        this.prix_unitaire = prix_unitaire;
+        this.category = category;
+
+    };
